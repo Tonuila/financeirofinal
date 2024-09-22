dfe206180e7f9a8b07413bf6d335dc836f540192
Tonuila <tonywillian400@gmail.com>
2024-09-22 16:35:54 -0300

    first commit

---
+++ src/pages/Sobre.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Sobre () {
+    return (
+    <div>
+        <h1>O criador do site</h1>
+        <h2>Tony Willian Althaus</h2>
+        <h3>Vendedor de carros e estudante de engenharia de software</h3>
+        <img src={'https://avatars.githubusercontent.com/u/141372737?v=4'} alt="imagem" /> <br />
+        <p>
+
+Olá! Meu nome é Tony Willian Althaus e trabalho em uma loja de carros, um ambiente que me permite estar em contato constante com o mundo fascinante do automobilismo. Desde sempre, tenho uma grande paixão por carros customizados, e tenho um Honda Civic 2002 que atualmente estou turbinando. <br />
+Além da minha dedicação ao universo dos carros, valorizo profundamente o tempo que passo com meus amigos e minha namorada, Kamila. Gosto de aproveitar ao máximo esses momentos de descontração e diversão, seja participando de eventos automobilísticos, organizando encontros com amigos ou simplesmente relaxando com ela. <br />
+Acredito que a combinação de minha paixão por carros, o convívio com pessoas queridas e o desejo de sempre aprender e crescer são o que tornam a minha vida tão rica e gratificante. Estou sempre em busca de novos desafios e experiências que me ajudem a evoluir tanto pessoal quanto profissionalmente. <br />
+Atualmente, estou cursando Engenharia de Software, um campo que me permite explorar a criação e a otimização de sistemas e aplicações. A faculdade tem sido uma jornada desafiadora e gratificante, oferecendo-me a chance de desenvolver habilidades técnicas e resolver problemas complexos, o que complementa minha experiência profissional e me prepara para uma carreira dinâmica na área de tecnologia.
+
+        </p>
+        <a href="https://github.com/Tonuila" target="_blank" rel="noopener noreferrer">
+        GITHUB
+        <br />
+        </a>
+    </div>
+    )
+}
+export default Sobre;
+ 
